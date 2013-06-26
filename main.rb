33fdc1e505181a7d5d4c1acf670f3d9d92b31a2e
Alina Guzman <alinochka8@gmail.com>
2013-06-26 19:21:48 -0400

    Added a user input form that links to a movie/title page

--- main.rb
+++ main.rb
@@ -4,5 +4,19 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
+
+movie = Imdb::Search.new("title").movies.first
+
 get '/' do
+  @title = params[:title]
+  if @title
+      redirect to("/movies/#{@title.gsub(/\s/,'-')}")
+  else
+    ""
+  end
+  erb :root
+end
+
+get '/movies/:title' do
+  erb :movie
 end
