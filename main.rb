51deff192797749ef30c6ac583c8053e8c1f9747
Zachariah Reitano <zreitano@gmail.com>
2013-06-26 21:33:05 -0400

    Add movie only when another movie is added

--- main.rb
+++ main.rb
@@ -4,5 +4,50 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
+
+
+
 get '/' do
+  list_of_favorite_movies = File.new('movies.csv', 'r')
+  @movies = []
+  list_of_favorite_movies.each do |line|
+    @movies << line.split('|')
+  end
+  erb :home
+end
+
+get '/movies/:title' do
+@title = params[:title]
+list_of_favorite_movies = File.new('movies.csv', 'r')
+@movie_info_array = []
+  list_of_favorite_movies.each do |line|
+    if line.split('|')[0] == @title then
+      @movie_info_array = line.split('|')
+    end
+  end
+  erb :movie
+end
+
+
+get '/new_movie' do
+  erb :input
+end
+
+post '/new_movie' do
+  @title = params[:title]
+  new_movie= Imdb::Search.new(@title).movies.first
+  @company = new_movie.company
+  @genres = new_movie.genres
+  @length = new_movie.length
+  @director = new_movie.director
+  @mpaa_rating = new_movie.mpaa_rating
+  @plot_synopsis = new_movie.plot_synopsis
+  @tagline = new_movie.tagline
+  @year = new_movie.year
+  @poster = new_movie.poster
+  @release_date = new_movie.release_date
+  #erb :input
+  new_movie = File.new('movies.csv', 'a+')
+  new_movie.puts("#{@title}|#{@year}|#{@director}|#{@tagline}|#{@poster}")
+  redirect to("/movies/#{URI::encode @title}")
 end
