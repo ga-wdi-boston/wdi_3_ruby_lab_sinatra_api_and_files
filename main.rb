b89c704a33a52d88ba540d3b8b885c142a9bfa81
Ben Israeli <jisraeli@gmail.com>
2013-06-26 23:40:53 -0400

    Created movies.erb, started main.rb

--- main.rb
+++ main.rb
@@ -4,5 +4,13 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
+
 get '/' do
+    @movies = []
+  movie_file = File.new("movies.csv", "r")
+  movie_file.each do |line|
+    @movies << line.split(',')
+
+  end
+erb :movies
 end
