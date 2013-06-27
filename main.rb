c0c6a8f0dd338a9e29abe8a043b23fe684cfe574
Beryl <beryl.schragger@gmail.com>
2013-06-27 01:29:02 -0400

    Add code for index page, index list, and add movie form.

--- main.rb
+++ main.rb
@@ -5,4 +5,33 @@ require 'sinatra/reloader' if development?
 require 'imdb'
 
 get '/' do
+  f = File.new("movies.csv", "r")
+  @movies = []
+  f.each do |line|
+    if line
+      @movies << line
+    end
+  end
+  erb :index
 end
+
+get '/movie/:movie_name' do
+end
+
+# Use the first search result. Here I'm searching for the "Jobs" movie.
+my_movie = Imdb::Search.new("Jobs").movies.first
+puts my_movie.title
+puts my_movie.poster
+puts my_movie.year
+
+#### Methods on an individual movie
+#company
+#genres (multiple)
+#length
+#director
+#mpaa_rating
+#plot_synopsis
+#tagline
+#year
+#poster
+#release_date
