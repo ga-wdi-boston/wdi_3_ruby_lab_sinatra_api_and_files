1f61957c3ca7e97c9a2888c3ae5dbca25d8bc77b
Ben Israeli <jisraeli@gmail.com>
2013-06-27 00:16:38 -0400

    added newmovie.erb and moive.erb plus get methods to main.rb accidentally wrote this comment for wrong commit earlier on twitter

--- main.rb
+++ main.rb
@@ -7,10 +7,21 @@ require 'imdb'
 
 get '/' do
     @movies = []
-  movie_file = File.new("movies.csv", "r")
+  movie_file = File.new('movies.csv', 'r')
   movie_file.each do |line|
     @movies << line.split(',')
-
   end
 erb :movies
+end
+
+get '/movie/:title' do
+  @title = params[:title]
+
+get '/new_movie' do
+erb :new_movie
+end
+
+post '/new_movie' do
+  @movie = Imdb::Search.new(@title).movies.first
+erb :movie
 end
