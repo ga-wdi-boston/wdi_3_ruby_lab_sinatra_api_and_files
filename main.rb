db76dea9fb284c097cca35214da1aeb8a4253163
Christopher Wright <cmwright33@gmail.com>
2013-06-27 10:24:09 -0400

    corrected syntax and populated both movies and movie title pages

--- main.rb
+++ main.rb
@@ -7,35 +7,38 @@ require 'imdb'
 
 
 get '/new_movie' do
-  @new_movie = params[:new_movie]
-  movies_file = File.new("movies.csv", "a+")
-  my_movie = Imdb::Search.new("superman").movies.first
-
-  film = []
-  film << my_movie.title
-  film << my_movie.poster
-  film << my_movie.year
-  movies_file << film
-
-  movies_file.close
-  erb :new_movie
-end
+      @new_movie = params[:new_movie]
+      movies_file = File.new("movies.csv", "a+")
+      my_movie = Imdb::Search.new("Braveheart").movies.first
+      @title = my_movie.title
+      @poster = my_movie.poster
+      @year = my_movie.year
+      movies_file.puts("#{@title}| #{@year} | #{@poster}")
+      movies_file.close
+      erb :new_movie
+    end
 
 
 
-post '/new_movie' do
+get '/new_movie' do
 
  erb :new_movie
 end
 
+post '/new_movie' do
+  @title = params[:title]
+  @movie = Imdb::Search.new(@title).movies.first
+  File.new("movies.csv", "a+") do |movie_info|
+  movie_info.puts("")
+  erb :movie
+end
 
 
 get '/movies' do
   movies_file = File.new('movies.csv', 'r')
   @movies = []
   movies_file.each do |movie|
-    @movies << movie[0]
-    @movies.flatten
+  @movies << movie.split(',')[0]
   end
   movies_file.close
   erb :movies
@@ -45,9 +48,19 @@ get '/movie/:name' do
 
 end
 
+
+
+  # redirect to("/movies/#{@title}")
+
 # def movie_getter()
 # my_movie = Imdb::Search.new(name).movies.first
 # puts my_movie.title
 # puts my_movie.poster
 # puts my_movie.year
 # end
+
+  # film = []
+  # film << my_movie.title
+  # film << my_movie.poster
+  # film << my_movie.year
+  # movies_file << film
---
+++ morning_review.rb
@@ -0,0 +1,37 @@
+
+
+
+
+
+get '/' do
+
+end
+
+  # by analogy
+  # '/l/addition/2'
+  # get '/:first/:operation/:second'
+  # param[:first].to_f
+
+
+post '/newmovie/:title' do
+  # the user has posted data from the form,
+  # imdb api to go search for the title
+  # @ titile is a string
+  @title = params[:titile]
+
+  # A movie with the title "@title"
+  @movie = Imdb::Search.new("Batman").movies.first
+  # Don't forget that you can debut this way
+  # puts "Movie details:"
+    puts @movie
+
+  end
+
+title = @movie.title
+year = @movie.year
+
+output_file = File.new('movies.csv', 'a+')
+output_file.puts("#{@title}| #{@year}")
+output_file.close
+
+rediret to '/'
