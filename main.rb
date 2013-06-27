7e0148f6cd7565d443d324201962b7e2f98b4cfe
Tim <tim.blonski@gmail.com>
2013-06-27 13:48:19 -0400

    added the 'add movie button'

--- main.rb
+++ main.rb
@@ -17,6 +17,24 @@ get '/favorite_movies' do
 end
 
 
+
+get '/add_movie' do
+@movie = params[:movie]
+my_movie = Imdb::Search.new(@movie).movies.first
+@title = my_movie.title
+@plot_summary = my_movie.company
+@length = my_movie.length
+@director = my_movie.director.first
+
+output_file = File.new('movies.csv', 'a+')
+output_file.puts("#{@title},#{@company},#{@length},#{@director}")
+output_file.close
+
+redirect to "/favorite_movies/#{@movie}"
+end
+
+
+
 get '/favorite_movies/:movie' do
   @movie = params[:movie]
   movie_file = File.new("movies.csv", "r")
@@ -32,15 +50,18 @@ end
 
 
 
-  #  get '/form' do
-  # @movie = params[:movie]
-  #    if @movie
-  #   redirect to("/#{@movie}")
+#    get '/form' do
+#   @movie = params[:movie]
+#      if @movie
+#     redirect to("/#{@movie}")
 #   else
 #     puts "Enter a valid username"
 #   end
 #   erb :form
 # end
+
+
+
 
 # get '/:movie' do
 #   @movie = params[:movie]
@@ -51,7 +72,7 @@ end
 # end
 
 # title = @movie.title
-# plot_summary = @movie.title
+# plot_summary = @movie.plot_summary
 
 # output_file = File.new('movies.csv', 'a+')
 
