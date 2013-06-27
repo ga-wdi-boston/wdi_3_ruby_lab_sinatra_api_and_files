edc752bbd96b53ee184c5f50e6ccc2c1ed5804f9
Theodore Cleveland <tcclevela@gmail.com>
2013-06-26 23:03:45 -0400

    added movie.erb, worked on file-reading

--- main.rb
+++ main.rb
@@ -5,14 +5,30 @@ require 'sinatra/reloader' if development?
 require 'imdb'
 
 get '/' do
+	@favorites = {}
+	f= File.new('movies.csv', 'r')
+	f.each do |line|
+		@favorites[line.split(',').to_sym] = line.split(',')
+	end
 	erb :favorites
 end
 
 get '/new_favorite/?:new_movie?' do
-	if params[:new_movie]
-		@new_movie = params[:new_movie]
-	else
-		@new_movie = " "
+	if params[:new_movie] then
+		f= File.new('movies.csv','r')
+		@new_movie = Imbd::Search.new(:new_movie).movies.first
+		redirect to('new_favorite/URI::encode(:new_movie)')
 	end
 	erb :new_favorite
+end
+
+
+get '/movie/:movie_title' do
+	f = File.new('movies.csv', 'r')
+	f.each do |line|
+		if line.split(',')[0] == movie_title
+			@movie = line
+			erb :movie
+		end
+	end
 end
