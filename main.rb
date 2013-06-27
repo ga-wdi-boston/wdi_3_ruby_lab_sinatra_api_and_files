32d35da73328596c10793b7a1eb9a83cae134f9b
Christine Coulter <christinetcoulter@gmail.com>
2013-06-26 22:50:01 -0400

    Add get / route to main.rb . Use example movies in movies.csv

--- main.rb
+++ main.rb
@@ -4,6 +4,16 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
+# This should list all the movies
 get '/' do
+  movie_file = File.new('movies.csv', 'r')
+  @movies_hash = {}
+  movie_file.each do |line|
+    movie_array = line.split(',')
+    title = movie_array[0]
+    @movies_hash[title] = movie_array
+  end
+  movie_file.close
+  erb :movies
 end
 
