2d55fdde2a07665f19f2f55dc3db0b8fdbeb9a44
alicem <alice.mottola@gmail.com>
2013-06-26 23:40:37 -0400

    Start

--- main.rb
+++ main.rb
@@ -5,4 +5,13 @@ require 'sinatra/reloader' if development?
 require 'imdb'
 
 get '/' do
+
+  @movies = []
+  movie_file = File.new('movies.csv', 'r')
+  movie_file.each do |line|
+    @movies << line.split(',')
+  end
+
+  erb :movies
+
 end
