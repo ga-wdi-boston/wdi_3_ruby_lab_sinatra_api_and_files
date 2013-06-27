9877626df4d215f828d3bb4f71995271eb44fcb0
Christine Coulter <christinetcoulter@gmail.com>
2013-06-27 00:18:46 -0400

    Add use of IMDB API. Able to add movie information to movies.csv file.

--- main.rb
+++ main.rb
@@ -36,3 +36,32 @@ end
 get '/new_movie' do
   erb :new_movie
 end
+
+# Create a new movie by sending a POST request to this URL
+post '/new_movie' do
+  @title = params[:title]
+  new_movie = Imdb::Search.new(@title).movies.first
+
+  @movie_title = new_movie.title
+  @company = new_movie.company
+  @genres = new_movie.genres
+  @length = new_movie.length
+  @director = new_movie.director
+  @mpaa_rating = new_movie.mpaa_rating
+  @plot_synopsis = new_movie.plot_synopsis
+  @tagline = new_movie.tagline
+  @year = new_movie.year
+  @poster= new_movie.poster
+  @release_date = new_movie.release_date
+
+
+  f = File.new('movies.csv', 'a+')
+  f.puts("#{@movie_title}|#{@company}|#{@genres}|#{@length}|#{@director}|#{@mpaa_rating}|#{@plot_synopsis}|#{@tagline}|#{@year}|#{@poster}|#{@release_date}")
+  f.close
+
+  #This will send you to the newly created movie
+  redirect to("/movie/#{URI::encode(@title)}")
+end
+
+
+
