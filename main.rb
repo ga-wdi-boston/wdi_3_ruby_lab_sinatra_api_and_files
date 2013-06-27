5be86c477c63b4b31367472ed219b8256f9c4731
Theodore Cleveland <tcclevela@gmail.com>
2013-06-26 23:45:48 -0400

    added layout nav and changed main to check for duplicate favorite entries

--- main.rb
+++ main.rb
@@ -4,29 +4,49 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
-get '/' do
+helpers do
+	# as great as "Jobs (2013) looks, I only want one copy"
+	def movie_exists?(title)
+		f = File.new('movies.csv', 'r')
+		f.each do |line|
+			return true if line.split('|')[0] == title
+		end
+		false
+	end
+end
+
+
+get '/about' do
 	@favorites = {}
 	f= File.new('movies.csv', 'r')
 	f.each do |line|
-		@favorites[line.split(',').to_sym] = line.split(',')
+		@favorites[line.split('|').to_sym] = line.split(',')
 	end
 	erb :favorites
 end
 
 get '/new_favorite/?:new_movie?' do
 	if params[:new_movie] then
-		f= File.new('movies.csv','r')
-		@new_movie = Imbd::Search.new(:new_movie).movies.first
-		redirect to('new_favorite/URI::encode(:new_movie)')
+		f = File.new('movies.csv', 'a+')
+		@new_movie = Imdb::Search.new(params[:new_movie]).movies.first
+		@title = @new_movie.title
+		@year = @new_movie.year
+		@director = @new_movie.director
+		@poster = @new_movie.poster
+		@rating = @new_movie.rating
+		f.puts "#{@title}| #{@year}| #{@director}| #{@poster}|#{@rating}\n" unless movie_exists?(@title)
+		f.close
+		redirect to("/movie/#{URI::encode(@title)}")
+	else
+		erb :new_favorite
 	end
-	erb :new_favorite
 end
 
 
 get '/movie/:movie_title' do
 	f = File.new('movies.csv', 'r')
 	f.each do |line|
-		if line.split(',')[0] == movie_title
+		if line.split('|')[0] == params[:movie_title]
 			@movie = line
 			erb :movie
 		end
