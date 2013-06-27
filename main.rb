2faab18eec68ed78e116b9685e80fd1b7ddf8807
Christine Coulter <christinetcoulter@gmail.com>
2013-06-26 23:34:16 -0400

    Add get movie/name route block and add structure to movie.erb file

--- main.rb
+++ main.rb
@@ -4,16 +4,31 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
-# This should list all the movies
-get '/' do
+
+# Function to prep movie information
+###remember to change split to |
+def get_movie_hash
   movie_file = File.new('movies.csv', 'r')
-  @movies_hash = {}
+  movies_hash = {}
   movie_file.each do |line|
     movie_array = line.split(',')
     title = movie_array[0]
-    @movies_hash[title] = movie_array
+    movies_hash[title] = movie_array
   end
   movie_file.close
+  return movies_hash
+end
+
+
+# This should list all the movies
+get '/' do
+  @movies = movies_hash
   erb :movies
+end
+
+# This should show a single movie_array
+get '/movie/:name' do
+  @name = params[:name]
+  erb :movie
 end
 
