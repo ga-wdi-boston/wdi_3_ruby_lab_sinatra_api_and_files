54c96e11506056341810faa94c7260ef4748be86
tommyb67 <tommybrennan@gmail.com>
2013-06-27 09:04:55 -0400

    did not get to far.  Doing a commit b4 Chris does the review

--- main.rb
+++ main.rb
@@ -4,9 +4,36 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
+# I want to populate the movie.csv file with info from Imdb
+# This file should also be writable to add movies later
+# I will then create a hash using the supplied methods as keys
+
+
 get '/' do
-  erb :movie_output
 end
+
+#def my_movie_array()
+  # Open the movies.csv file
+  #File.New('movies.csv', 'a+'){ |outfile|
+    CSV.open('movies.csv' 'ab') do  |csv|
+      csv << ['jobs', 'company']
+  end
+  erb :movie_output
+#end
+
+
+
+
+  # create an empty hash
+ # my_movie_hash = {}
+
+#end
+
+
+
+
+
+
 
 
 
