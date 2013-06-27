67a90761768d1fe5696bf933969504138afb43a5
Jamal Powell <jamalkpowell@gmail.com>
2013-06-27 10:16:36 -0400

    added movies into csv file

--- main.rb
+++ main.rb
@@ -5,4 +5,36 @@ require 'sinatra/reloader' if development?
 require 'imdb'
 
 get '/' do
+
+@movie_title = params[:movie_title]
+if @movie_title
+    redirect to "/movies/#{@movie_title}"
+  else
+    "What was that? Try another movie"
+  end
+  erb :search
 end
+
+get '/movies/:movie_title' do
+  @movie_title = params[:movie_title]
+  @movie = Imdb::Search.new(@movie_title)
+
+  erb :movie
+
+end
+
+# post '/new_movie/:movie_title' do
+#   @title = params[:movie_title]
+#   @movie =
+#   puts "Movie Details"
+#   return @movies.inspect
+
+#   erb :search
+# end
+
+# title = @movie.title
+# plot_summary = @movie.plot_summary
+
+# output_file = File.new('movies.csv', 'a+')
+# output_file.puts(#{"title"}, #{plot_summary})
+# output_file.close
